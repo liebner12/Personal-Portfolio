aa38352f4ffc4482e80c086c38ffd9594335cd6a
liebner12 <liebner15@gmail.com>
2020-07-09 18:58:39 +0200

    Pure HTML, CSS, JS website.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 let icon = document.getElementById("nav-icon");
 let sidebar = document.getElementById("sidenav");
 let nav_links = document.getElementsByClassName("nav-item");
+let boxes = document.getElementById("box-wrap");
 
 icon.addEventListener("click", () => {
     icon.classList.toggle("change");
@@ -14,4 +15,27 @@ for(let i = 0; i < nav_links.length; i++){
     })
 }
 
-
+(function() {
+    let elements;
+    let window_height;
+  
+    function init() {
+        elements = document.querySelectorAll('.box');
+        window_height = window.innerHeight;
+    }
+  
+    function checkPosition() {
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            let position_top = elements[i].getBoundingClientRect().top;
+  
+            if (position_top - window_height <= 0) {
+                element.classList.add('fade-in');
+            }
+        }
+    }
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+    init();
+    checkPosition();
+})();
